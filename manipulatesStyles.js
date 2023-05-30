105f317a19d574017cf4e3a058061ed40378085e
Rajesh912152 <121996259+Rajesh912152@users.noreply.github.com>
2023-05-30 11:07:15 +0530

    Add files via upload

---
+++ manipulatesStyles.js
@@ -0,0 +1,14 @@
+let headingElement = document.getElementById("changeHeading");
+
+function manipulateColor() {
+    headingElement.textContent = "Raj Technologies";
+
+    headingElement.style.color = "Blue";
+}
+
+function originalColor() {
+    headingElement.textContent = "Rajesh Technologies";
+
+    headingElement.style.color = "Black";
+
+}
